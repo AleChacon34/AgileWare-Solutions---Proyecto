41d69c82734f63d34a8b0994c37ccd64d94a5256
AleChacon <achacon81.ac@gmail.com>
2023-07-05 15:46:07 -0600

    Cambios en Nav e implementacion del Sweet Alert

---
+++ Bolsa de Empleo/Buscador de Empleo - Empleos/Aplicar oferta de empleo/aplicarEmpleo.js
@@ -0,0 +1,26 @@
+function onLoadComplete() {
+  let btn = document.querySelector("button");
+  btn.onclick = displayAlert;
+}
+
+function displayAlert() {
+  Swal.fire({
+    title: "¿Desea aplicar para este puesto?",
+    text: "Confirmar aplicacion de puesto",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Aplicar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Puesto aplicado con exito',
+        showConfirmButton: false,
+        timer: 1000
+      });
+    }
+  });
+}
