1ab95d43344658be591c9d08aefd7969a6243ac8
Jonah <jmorah@ucenfotec.ac.cr>
2023-07-08 16:18:24 -0600

    verificacion rol

--- Bolsa de Empleo/Sistema - Ingreso y Registro/Inicio de sesion/inicioSesion.js
+++ Bolsa de Empleo/Sistema - Ingreso y Registro/Inicio de sesion/inicioSesion.js
@@ -1,9 +1,11 @@
 "use strict";
 
+let autenticado = false;
+
 let usuarios = [{
         correo: "administrador@administrador.com",
         pass: "admin",
-        rol: "administrador"
+        rol: "Administrador"
     }, 
     {
         correo: "buscador@buscador.com",
@@ -20,33 +22,56 @@ document.addEventListener("DOMContentLoaded", () =>{
 function obtenerCredenciales(){
     let correo = document.querySelector("input[name='correo']");
     let pass = document.querySelector("input[name='pass']");
+    console.log( correo, pass);
     let usuario = {
         correo:correo.value,
-        pass:pass.value, 
+        pass:pass.value 
     }
     verificarDatosIngreso(usuario);
 }
 
 function verificarDatosIngreso(usuario){
+    let usuarioCorrecto;
     for (let user of usuarios){
         console.log(usuario.correo);
-        console.log(usuarios.correo);
-        if (usuario.correo === usuarios.correo){
+        console.log(usuario.pass);
+        if (usuario.correo === user.correo){
             alert("Usuario Funciona!!!");
-            if (usuario.pass === usuarios.pass){
-                alert("Contraseña correcta!!!");
-                paginaIncio(usuarios.rol);
+            if (usuario.pass === user.pass){
+                autenticado = true
+                usuarioCorrecto = user;
+                console.log(usuarioCorrecto.rol)
             }
-            else{
-                alert("Contraseña incorrecta!!!");
-            }       
         }
-        else{
-            alert("Usuario Incorrecto!!!");
-        }
-    }
+    }    
+    // if (autenticado){
+    //     paginaInicio(usuarioCorrecto.rol);
+    // }
 }
 
-function paginaIncio(){
-    window.location.href = "#";
-}
+// function paginaInicio(rol){
+//     if (rol === "Buscador de Empleo"){
+//         window.location.href = "/AgileWare-Solutions---Proyecto/Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Perfil usuario/perfilUsuario.html";
+//     }
+
+//     else if(rol === "Administrador"){
+//         window.location.href = "facebook.com";
+//     }
+
+//     else if(rol === "Reclutador"){
+//         window.location.href = "google.com";
+//     }
+
+//     else if(rol === "Manager"){
+//         window.location.href = "192.168.0.1";
+//     }
+
+//     else if(rol === "Super-Usuario"){
+//         window.location.href = "twitter.com";
+//     }
+
+//     else if(rol === "Empresa"){
+//         window.location.href = "/AgileWare-Solutions---Proyecto/Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Perfil usuario/perfilUsuario.html";
+//     }
+// }
+        
