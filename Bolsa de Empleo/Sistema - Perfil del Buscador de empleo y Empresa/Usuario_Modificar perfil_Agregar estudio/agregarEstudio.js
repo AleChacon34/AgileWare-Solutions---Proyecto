804ba5724b6a1521be982126011deefdb38fe04e
AleChacon <achacon81.ac@gmail.com>
2023-07-12 16:03:29 -0600

    Updates

---
+++ Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Usuario_Modificar perfil_Agregar estudio/agregarEstudio.js
@@ -0,0 +1,18 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    let btn = document.querySelector("#añadirEstudio");
+    btn.onclick = redirect;
+
+    let btn2 = document.querySelector('#agregar');
+    btn2.onclick = displayAlert;
+});
+
+function redirect() {
+    location.href = "agregarEstudio.html";
+}
+
+function displayAlert() {
+    Swal.fire('¡Invitacion enviada!', '', 'success');
+    location.href = "UsuarioModificarPerfilAgregarEstudio.html";
+}
