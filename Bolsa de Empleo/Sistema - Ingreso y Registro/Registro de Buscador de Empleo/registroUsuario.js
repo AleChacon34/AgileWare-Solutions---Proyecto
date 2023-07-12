3187ea2f20a109abe54277121b3515777a025737
jmorahU96 <jmorah@ucenfotec.ac.cr>
2023-07-12 14:30:49 -0600

    terminar funcionalidad y crear pagina de Inicio

--- Bolsa de Empleo/Sistema - Ingreso y Registro/Registro de Buscador de Empleo/registroUsuario.js
+++ Bolsa de Empleo/Sistema - Ingreso y Registro/Registro de Buscador de Empleo/registroUsuario.js
@@ -7,20 +7,36 @@ document.addEventListener("DOMContentLoaded", () =>{
     registro.addEventListener("click", completarRegistro);
 })
 
-
-function registrarInformacion(){
+function completarRegistro(){
     let nombre = document.querySelector("input[name='nombre-usuario']");
     let apellidos = document.querySelector("input[name='apellidos-usuario']");
     let correo = document.querySelector("input[name='correo']");
     let telefono = document.querySelector("input[name='numero-telefono']");
-    let genero = document.querySelector("input[name='genero']");
+    let genero = document.querySelector("#seleccion-genero");
     let contrasenna = document.querySelector("input[name='contrasenna']");
-    console.log(nombre, apellidos, correo, telefono, genero, contrasenna); 
+    let usuario = {nombreBuscador: nombre.value, apellidosBuscador: apellidos.value, correoBuscador: correo.value, telefonoBuscador: telefono.value, generoBuscador: genero.value, contrasennaBuscador: contrasenna.value,};
+    console.log(usuario);
+    guardarRegistro(listaBuscadoresdeEmpleo, usuario);
 }
 
-function completarRegistro(nombre, apellidos, correo, telefono, genero, contrasenna){
-    console.log("Funciona!!");
-    let usuario = {nombreBuscador: nombre, apellidosBuscador: apellidos, correoBuscador: correo, telefonoBuscador: telefono, generoBuscador: genero, contrasennaBuscador: contrasenna,};
-    listaBuscadoresdeEmpleo.push(usuario);
+function guardarRegistro(listaUsuarios, infoUsuario){
+
+    if (!((infoUsuario.nombreBuscador === "") || (infoUsuario.apellidosBuscador === "") || (infoUsuario.correoBuscador === "") || (infoUsuario.telefonoBuscador === "") || (infoUsuario.generoBuscador === "") || (infoUsuario.contrasennaBuscador === ""))){
+        listaUsuarios.push(infoUsuario);
+        Swal.fire({
+            icon: 'success',
+            title: 'Registro exitoso',
+            text: 'La información del nuevo usuario a sido guardada exitosamente.',
+        })
+    }
+
+    else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Información faltante',
+            text: 'Por favor llene todos los espacios para completar el registro.',
+        })
+    }
+        
 }
     
