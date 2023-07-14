2dbb852c3d90212fb748b0082d580c39d1b9a515
AleChacon <achacon81.ac@gmail.com>
2023-07-14 12:49:55 -0600

    Updates

--- Bolsa de Empleo/jsGeneral/cerrarSesion.js
+++ Bolsa de Empleo/jsGeneral/cerrarSesion.js
@@ -1,10 +1,16 @@
 "use strict";
 
+/**
+ * Al cargarse las paginas, llama al evento del boton de cierre de sesion
+ */
 document.addEventListener("DOMContentLoaded", () => {
-  let anchor = document.querySelector('#logOut');
-  anchor.onclick = logOut;
+  let btn = document.querySelector('#logOut');
+  btn.onclick = logOut;
 });
 
+/**
+ * Funcion que se encargar de redirigir a la pagina de inicio al dar 'click' en el boton de cierre de sesion
+ */
 function logOut() {
   location.replace("/Bolsa de Empleo/Pagina de Inicio/paginaInicio.html");
 }
--- Bolsa de Empleo/jsGeneral/confirmacionActualizar.js
+++ Bolsa de Empleo/jsGeneral/confirmacionActualizar.js
@@ -1,10 +1,16 @@
 "use strict";
 
+/**
+ * Al cargar la pagina, ejecuta el evento asignado al boton
+ */
 document.addEventListener("DOMContentLoaded", () =>{
     let actualizarBtn = document.querySelector("button[name='actualizar-perfil']");
     actualizarBtn.addEventListener("click", notificarActualizar);
 })
 
+/**
+ * Funcion que muestra la alerta de que se actualizaron los datos
+ */
 function notificarActualizar(){
     console.log("funciona")
     Swal.fire({
