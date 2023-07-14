56649b6a0d159824b428619c857db8f83ca2bb00
AleChacon <achacon81.ac@gmail.com>
2023-07-14 12:57:32 -0600

    Updates

--- Bolsa de Empleo/Buscador de Empleo - Empleos/Visualizar empleos disponibles/visualizarEmpleos.js
+++ Bolsa de Empleo/Buscador de Empleo - Empleos/Visualizar empleos disponibles/visualizarEmpleos.js
@@ -1,11 +1,17 @@
-function onLoadComplete() {
+/**
+ * Al cargar la pagina, se le asigna el evento a los botones dentro del html (excepto al del nav)
+ */
+document.addEventListener('DOMContentLoaded', () => {
     let btns = document.querySelectorAll('.visualizar');
     for (let i = 0; i < btns.length; i++) {
         let btn = btns[i];
         btn.onclick = displayOffer;
     }
-}
+});
 
+/**
+ * Funcion que redirige a la pagina de aplicar empleo
+ */
 function displayOffer() {
     window.location.href = "../Aplicar oferta de empleo/aplicarEmpleo.html";
 }
