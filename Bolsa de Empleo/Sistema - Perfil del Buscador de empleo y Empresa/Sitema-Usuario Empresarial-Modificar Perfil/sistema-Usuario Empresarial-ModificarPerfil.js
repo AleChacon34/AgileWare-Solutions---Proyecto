33aed9457066a254088424b41c0c84c572cc340a
Jonah <jmorah@ucenfotec.ac.cr>
2023-07-13 20:54:12 -0600

    crear js para actualizar perfil

--- Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Sitema-Usuario Empresarial-Modificar Perfil/sistema-Usuario Empresarial-ModificarPerfil.js
+++ Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Sitema-Usuario Empresarial-Modificar Perfil/sistema-Usuario Empresarial-ModificarPerfil.js
@@ -1,5 +1,7 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded" () =>{
+document.addEventListener("DOMContentLoaded", () =>{
     actualizarBtn = document.querySelector("button[name='actualizar-perfil']");
+    actualizarBtn.addEventListener("click", notificarActualizar);
 })
+
