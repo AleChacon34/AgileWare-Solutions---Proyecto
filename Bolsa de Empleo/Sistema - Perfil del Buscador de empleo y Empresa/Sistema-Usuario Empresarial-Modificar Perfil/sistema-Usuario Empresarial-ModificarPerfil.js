aae16629da247342f8370c565590fa516f0c6a77
AleChacon <achacon81.ac@gmail.com>
2023-07-13 21:05:54 -0600

    Update

---
+++ Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Sistema-Usuario Empresarial-Modificar Perfil/sistema-Usuario Empresarial-ModificarPerfil.js
@@ -0,0 +1,7 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () =>{
+    actualizarBtn = document.querySelector("button[name='actualizar-perfil']");
+    actualizarBt.addEventListener("click", notificarActualizar);
+})
+
