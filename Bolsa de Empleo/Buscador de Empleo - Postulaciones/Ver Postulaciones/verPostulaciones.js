d23c3af7089db4da55d2b1b700233cbce015bba7
AleChacon <achacon81.ac@gmail.com>
2023-07-14 12:59:13 -0600

    Update

--- Bolsa de Empleo/Buscador de Empleo - Postulaciones/Ver Postulaciones/verPostulaciones.js
+++ Bolsa de Empleo/Buscador de Empleo - Postulaciones/Ver Postulaciones/verPostulaciones.js
@@ -1,11 +1,17 @@
-function onLoadComplete() {
+/**
+ * Al cargar la pagina, se le asigna el evento a los botones
+ */
+document.addEventListener('DOMContentLoaded', () => {
     let btns = document.querySelectorAll('.verPostulacion');
     for (let i = 0; i < btns.length; i++) {
         let btn = btns[i];
         btn.onclick = redirect;
     }
-}
+});
 
+/**
+ * Funcion que redirige a la pagina de la postulacion
+ */
 function redirect() {
     window.location.href = "postulacion.html";
 }
