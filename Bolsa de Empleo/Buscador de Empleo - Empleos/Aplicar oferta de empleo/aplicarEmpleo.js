6e972ac544c5205bab0fd098dec4cf143910ff65
AleChacon <achacon81.ac@gmail.com>
2023-07-14 12:53:55 -0600

    Updates

--- Bolsa de Empleo/Buscador de Empleo - Empleos/Aplicar oferta de empleo/aplicarEmpleo.js
+++ Bolsa de Empleo/Buscador de Empleo - Empleos/Aplicar oferta de empleo/aplicarEmpleo.js
@@ -1,14 +1,22 @@
-function onLoadComplete() {
+/**
+ * Al cargar la pagina, se le asigna el evento de la alerta al boton
+ */
+document.addEventListener('DOMContentLoaded', () => {
   let btn = document.querySelector("#aplicar");
   btn.onclick = displayAlert;
-}
+});
 
+/**
+ * Funcion uqe despliega la alerta con dos opciones
+ * Aplicar a puesto o Cancelar
+ */
 function displayAlert() {
   Swal.fire({
     title: "¿Desea aplicar para este puesto?",
     text: "Confirmar aplicacion de puesto",
     icon: "warning",
     showCancelButton: true,
+    cancelButtonText: "Cancelar",
     confirmButtonColor: "#3085d6",
     cancelButtonColor: "#d33",
     confirmButtonText: "Aplicar",
