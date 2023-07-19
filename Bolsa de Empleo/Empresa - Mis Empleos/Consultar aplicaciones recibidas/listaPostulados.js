e916b17dae0bc42f87c633a1de7ab68fe0ac36a9
Jonah <jmorah@ucenfotec.ac.cr>
2023-07-18 22:15:28 -0600

    genera tablas

--- Bolsa de Empleo/Empresa - Mis Empleos/Consultar aplicaciones recibidas/listaPostulados.js
+++ Bolsa de Empleo/Empresa - Mis Empleos/Consultar aplicaciones recibidas/listaPostulados.js
@@ -1,6 +1,6 @@
 'use strict';
 
-datos = [
+let datosUsuarios = [
     {nombre: "Jonathan", apellidos: "Mora Herra", correoElectronico: "jmorah@ucenfotec.ac.cr", estado: "Enviada"},
     {nombre: "Marco", apellidos: "Mora Herra", correoElectronico: "jmorah@ucenfotec.ac.cr", estado: "Enviada"},
     {nombre: "Cristopher", apellidos: "Mora Herra", correoElectronico: "jmorah@ucenfotec.ac.cr", estado: "Enviada"}
@@ -11,7 +11,28 @@ document.addEventListener("DOMContentLoaded", () =>{
     genBtn.addEventListener('click', generarReporte);
 });
 
-function generarReporte(datos){
-    document.createElement("table");
+function generarReporte(datosUsuarios){
+    //Crear una tabla, agregarla al body y asignarle una clase
+    let crearTabla = document.createElement("table");
+    crearTabla.classList.add("tabla-datos");
+    document.body.appendChild(crearTabla);
 
+    //Crear el Table Header
+    let headRow = crearTabla.insertRow()
+    headRow.insertCell(0).outerHTML ="<th>Nombre</th>";
+    headRow.insertCell(1).outerHTML ="<th>Apellidos</th>";
+    headRow.insertCell(2).outerHTML ="<th>Correo Electrónico</th>";
+    headRow.insertCell(3).outerHTML ="<th>Estado</th>";
+
+    //Crear las filas con la informacion de los objetos
+    for (let dato = 0;  dato < datosUsuarios.length; dato++){
+        console.log(datosUsuarios)
+        console.log(datosUsuarios.nombre)
+        console.log(datosUsuarios.apellidos)
+        let row = crearTabla.insertRow();
+        row.insertCell(0).appendChild(document.createTextNode(datosUsuarios[dato].nombre));
+        row.insertCell(1).appendChild(document.createTextNode(datosUsuarios[dato].apellidos));
+        row.insertCell(2).appendChild(document.createTextNode(datosUsuarios[dato].correoElectronico));
+        row.insertCell(3).appendChild(document.createTextNode(datosUsuarios[dato].estado));
+    }
 }
