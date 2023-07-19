43018c293fa06a0b5a2779d3f00241fdd92b6432
jmorahU96 <jmorah@ucenfotec.ac.cr>
2023-07-19 17:39:08 -0600

    update

--- Bolsa de Empleo/Empresa - Mis Empleos/Consultar aplicaciones recibidas/listaPostulados.js
+++ Bolsa de Empleo/Empresa - Mis Empleos/Consultar aplicaciones recibidas/listaPostulados.js
@@ -1,11 +1,5 @@
 'use strict';
 
-// let datosUsuarios = [
-//     {nombre: "Jonathan", apellidos: "Mora Herra", correoElectronico: "jmorah@ucenfotec.ac.cr", estado: "Enviada"},
-//     {nombre: "Marco", apellidos: "Mora Herra", correoElectronico: "jmorah@ucenfotec.ac.cr", estado: "Enviada"},
-//     {nombre: "Cristopher", apellidos: "Mora Herra", correoElectronico: "jmorah@ucenfotec.ac.cr", estado: "Enviada"}
-// ]
-
 document.addEventListener("DOMContentLoaded", () =>{
     let genBtn = document.querySelector("button[name='generar-reporte']");
     genBtn.addEventListener('click', generarReporte);
