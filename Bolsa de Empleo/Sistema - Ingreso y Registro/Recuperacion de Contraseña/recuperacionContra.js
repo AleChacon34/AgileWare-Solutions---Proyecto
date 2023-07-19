630b9269c242c9d93d6d30864911f8cf2a98bcf8
AleChacon <achacon81.ac@gmail.com>
2023-07-18 20:03:14 -0600

    Funcionalidad de recuperacion de contraseña

--- Bolsa de Empleo/Sistema - Ingreso y Registro/Recuperacion de Contraseña/recuperacionContra.js
+++ Bolsa de Empleo/Sistema - Ingreso y Registro/Recuperacion de Contraseña/recuperacionContra.js
@@ -1,15 +1,40 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () =>{
-    let actualizarBtn = document.querySelector("button[name='recuperar-contrasenna']");
-    actualizarBtn.addEventListener("click", notificarActualizar);
-})
+document.addEventListener("DOMContentLoaded", () => {
+  let actualizarBtn = document.querySelector(
+    "button[name='recuperar-contrasenna']"
+  );
+  actualizarBtn.addEventListener("click", getData);
+});
 
-function notificarActualizar(){
-    console.log("funciona")
+function getData() {
+  let email = document.getElementById('email').value;
+  notificarActualizar(email)
+}
+
+function notificarActualizar(email) {
+  /*console.log("funciona")
     Swal.fire({
         icon: "success",
         text: "Recibirá un correo con la información para la recuperación de contraseña.",
         confirmButtonText: "Continuar",
-    })
+    })*/
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Port: 2525,
+    Username: "mchaconc1@ucenfotec.ac.cr",
+    Password: "93F94DAF6A182203E3E6D1738AFA001293F9",
+    To: `${email}`,
+    From: "mchaconc1@ucenfotec.ac.cr",
+    Subject: "Pruebas de recuperacion de contrasennia",
+    Body: "Prueba de envio de correo con JS",
+  }).then((message) => {
+    Swal.fire({
+      icon: "success",
+      text: "Recibirá un correo con la información para la recuperación de contraseña.",
+      confirmButtonText: "Continuar",
+    });
+  }).catch(err => {
+    Swal.fire("Ha habido un error", "", "warning");
+  });
 }
