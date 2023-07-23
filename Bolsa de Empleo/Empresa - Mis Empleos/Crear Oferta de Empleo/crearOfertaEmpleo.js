d68fcfada3913767d442dc6e2f83ce4a7b3322cf
Jonah <jmorah@ucenfotec.ac.cr>
2023-07-22 19:36:14 -0600

    botones y sweetalert

--- Bolsa de Empleo/Empresa - Mis Empleos/Crear Oferta de Empleo/crearOfertaEmpleo.js
+++ Bolsa de Empleo/Empresa - Mis Empleos/Crear Oferta de Empleo/crearOfertaEmpleo.js
@@ -7,23 +7,27 @@ document.addEventListener("DOMContentLoaded", () =>{
     let ocultarBtn = document.querySelector("button[name='ocultar']");
     publicarBtn.addEventListener("click", publicarOferta);
     ocultarBtn.addEventListener("click", ocultarOferta);
+    let cuadroTitulo= document.querySelector("#seccion-titulo").maxLength("10")
+    console.log(cuadroTexto)
 })
 
 function publicarOferta(){
     Swal.fire({
+        customClass: {popup: "swal"},
         icon: 'success',
         title: 'Oferta publicada con éxito',
         confirmButtonText: 'Continuar',
       }).then((result) => {
         if (result.isConfirmed) {
             window.location.replace("/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarListaOfertas.html")
-        }
-      })
+        };
+      });
 }
 
 function ocultarOferta(){
     console.log("oculta")
     Swal.fire({
+        customClass: {popup: "swal"},
         icon: 'success',
         title: 'Oferta ocultada con éxito',
         confirmButtonText: 'Continuar',
@@ -31,5 +35,5 @@ function ocultarOferta(){
         if (result.isConfirmed) {
             window.location.replace("/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarListaOfertas.html")
         }
-      })
+      });
 }
