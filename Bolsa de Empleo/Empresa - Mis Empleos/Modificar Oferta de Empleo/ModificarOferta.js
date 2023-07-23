9bf865460731fec7d16142d433841c65538de14a
Jonah <jmorah@ucenfotec.ac.cr>
2023-07-22 21:08:05 -0600

    updates

---
+++ Bolsa de Empleo/Empresa - Mis Empleos/Modificar Oferta de Empleo/ModificarOferta.js
@@ -0,0 +1,102 @@
+'use strict';
+
+//Agregar funcionalidades a todos los botones
+
+document.addEventListener("DOMContentLoaded", () =>{
+    let actualizarBtn = document.querySelector("#actualizar-btn");
+    actualizarBtn.addEventListener('click', actualizarOferta);
+    let eliminarBtn = document.querySelector("#eliminar-btn");
+    eliminarBtn.addEventListener('click', eliminarOferta);
+    let publicarBtn = document.querySelector("#publicar-btn");
+    publicarBtn.addEventListener('click', publicarOferta);
+    let ocultarBtn = document.querySelector("#ocultar-btn");
+    ocultarBtn.addEventListener('click', ocultarOferta);
+});
+
+// Funcion para actualizar los datos de la Oferta
+function actualizarOferta(){
+    Swal.fire({
+        icon: "warning",
+        title: 'Actualización de Oferta',
+        text: "¿Desea actualizar los datos de la oferta?",
+        confirmButtonText: "Actualizar",
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        Swal.fire({
+            icon: "success",
+            title: "Oferta actualizada con éxito",
+            timer: 2500,
+        }).then(() =>{
+            if(result.isConfirmed){
+                window.location.replace("/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarOferta.html");
+            };
+        });
+    });
+};
+
+// Funcion para eliminar la Oferta
+function eliminarOferta(){
+    Swal.fire({
+        icon: "warning",
+        title: 'Eliminación de Oferta',
+        text: "¿Desea eliminar los datos de la oferta?",
+        confirmButtonText: "Eliminar",
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        Swal.fire({
+            icon: "success",
+            title: "Oferta eliminada con éxito",
+            timer: 2500,
+        }).then(() =>{
+            if(result.isConfirmed){
+                window.location.replace("/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarOferta.html");
+            };
+        });
+    });
+};
+
+// Funcion para actualizar los datos de la Oferta
+function publicarOferta(){
+    Swal.fire({
+        icon: "warning",
+        title: '¿Desea publicar la oferta?',
+        text: "Esta oferta será visible públicamente",
+        confirmButtonText: "Publicar",
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        Swal.fire({
+            icon: "success",
+            title: "Oferta publicar con éxito",
+            timer: 2500,
+        }).then(() =>{
+            if(result.isConfirmed){
+                window.location.replace("/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarOferta.html");
+            };
+        });
+    });
+};
+
+// Funcion para eliminar la Oferta
+function ocultarOferta(){
+    Swal.fire({
+        icon: "warning",
+        title: '¿Desea ocultar la oferta?',
+        text: "Esta oferta dejará de ser visible públicamente",
+        confirmButtonText: "Ocultar",
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        Swal.fire({
+            icon: "success",
+            title: "Oferta ocultada con éxito",
+            timer: 2500,
+        }).then(() =>{
+            if(result.isConfirmed){
+                window.location.replace("/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarOferta.html");
+            };
+        });
+    });
+};
