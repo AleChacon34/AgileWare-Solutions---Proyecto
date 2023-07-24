6189aedc2807210af0600c846ee417501936c5b2
Jonah <jmorah@ucenfotec.ac.cr>
2023-07-24 15:58:37 -0600

    update

--- Bolsa de Empleo/Empresa - Colaboradores/Visualizar Colaboradores/visualizarManagers.js
+++ Bolsa de Empleo/Empresa - Colaboradores/Visualizar Colaboradores/visualizarManagers.js
@@ -1,4 +1,5 @@
 "use strict";
+let listaInvitaciones=[]; 
 
 document.addEventListener("DOMContentLoaded", ()=> {
   let btn = document.querySelector("#agregar-colaborador");
@@ -27,6 +28,9 @@ function insertEmail() {
   Swal.fire({
     title: 'Ingrese la dirección de correo electrónico',
     input: 'email',
+    customClass: {
+      input: "#textoUsuario"
+    },
     inputAttributes: {
       autocapitalize: 'off',
       placeholder: "ejemplo@ejemplo.com"
@@ -36,7 +40,14 @@ function insertEmail() {
     showLoaderOnConfirm: true,
     preConfirm: (result) => {
       Swal.fire('¡Invitación enviada!', '', 'success');
+      agregarRegistroInvitacion(result)
     },
     allowOutsideClick: () => !Swal.isLoading()
   });
+}
+
+function agregarRegistroInvitacion(value){
+  let invitacion = {id: "#123", correoinvitado: value, correoEmisor: "Admin@admin.com", rol: "Administrador" };
+  listaInvitaciones.push(invitacion);
+  console.log(listaInvitaciones);
 }
--- Bolsa de Empleo/Empresa - Colaboradores/Visualizar Colaboradores/visualizarReclutadores.js
+++ Bolsa de Empleo/Empresa - Colaboradores/Visualizar Colaboradores/visualizarReclutadores.js
@@ -1,4 +1,5 @@
 "use strict";
+let listaInvitaciones=[];
 
 document.addEventListener('DOMContentLoaded', () => {
   let btn = document.querySelector("#agregar-colaborador");
@@ -25,7 +26,10 @@ function displayAlert() {
 function insertEmail() {
   Swal.fire({
     title: 'Ingrese la dirección de correo electrónico',
-    input: 'text',
+    input: 'email',
+    customClass: {
+      input: "#textoUsuario"
+    },
     inputAttributes: {
       autocapitalize: 'off',
       placeholder: "ejemplo@ejemplo.com"
@@ -35,7 +39,14 @@ function insertEmail() {
     showLoaderOnConfirm: true,
     preConfirm: (result) => {
       Swal.fire('¡Invitación enviada!', '', 'success');
+      agregarRegistroInvitacion(result)
     },
     allowOutsideClick: () => !Swal.isLoading()
   });
+}
+
+function agregarRegistroInvitacion(value){
+  let invitacion = {id: "#123", correoinvitado: value, correoEmisor: "Admin@admin.com", rol: "Administrador" };
+  listaInvitaciones.push(invitacion);
+  console.log(listaInvitaciones);
 }
