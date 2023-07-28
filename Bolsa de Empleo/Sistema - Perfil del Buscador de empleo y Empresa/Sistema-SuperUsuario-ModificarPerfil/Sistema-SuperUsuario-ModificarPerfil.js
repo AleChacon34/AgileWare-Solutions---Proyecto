e1d01082544b02ee5ca9d14c312ff779b0c58a92
AleChacon <achacon81.ac@gmail.com>
2023-07-27 21:49:01 -0600

    Update

---
+++ Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Sistema-SuperUsuario-ModificarPerfil/Sistema-SuperUsuario-ModificarPerfil.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+    let btnActualizar = document.querySelector('#btnActualizar');
+    btnActualizar.addEventListener('click', getData);
+});
+
+function getData() {
+    let 
+}
