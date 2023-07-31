139e9a4c7ea7f68a145e8bbc56c1a515861c0adb
jmorahU96 <jmorah@ucenfotec.ac.cr>
2023-07-31 16:48:01 -0600

    update

--- Bolsa de Empleo/jsGeneral/barraNavegacionBuscador.js
+++ Bolsa de Empleo/jsGeneral/barraNavegacionBuscador.js
@@ -1,17 +1,19 @@
 'use strict'
-
+//Load Page
 document.addEventListener("DOMContentLoaded", ()=>{
     crearBarraNavegacion();
 });
 
 function crearBarraNavegacion(){
-    let navBar = document.createElement('navBar');
+    // Create NAV element and assign class
+    let htmlBody = document.querySelector("body");
+    let navBar = document.createElement('nav');
     navBar.classList.add("navBar");
-    let addDivI = navBar.appendChild(Object.assign(
-        document.createElement('div'),
-        {
-            classList: "navDiv",
-            innerHTML: "div <p>Hola</p>"
-        }
-    ));
+    //Append NAV to BODY
+    htmlBody.appendChild(navBar);
+    let addDivI = document.createElement('div');
+    let addIcon = addDivI.appendChild(document.createElement("i"));
+    addIcon.classList.add("fa-solid","fa-user");
+
+    navBar.appendChild(addDivI);
 }
