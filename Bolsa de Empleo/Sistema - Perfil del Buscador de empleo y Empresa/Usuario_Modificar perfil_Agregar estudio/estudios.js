661d891a06e383cbce6b9b1dd1e22665a1059be5
jaquisg <jaquisolisg@gmail.com>
2023-07-31 16:32:43 -0600

    cambios

--- Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Usuario_Modificar perfil_Agregar estudio/estudios.js
+++ Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Usuario_Modificar perfil_Agregar estudio/estudios.js
@@ -1,8 +1,15 @@
 "use strict";
-
+//este codigo es para añadir estudio//
 document.addEventListener('DOMContentLoaded', () => {
     let btn = document.querySelector('#añadirEstudio');
     btn.onclick = () => {
         location.href = "agregarEstudio.html"
     };
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    let modBtn = document.querySelector('#modificar');
+    modBtn.addEventListener("click",direct) 
+})
+var 
+location.href = "agregarEstudio.html"
