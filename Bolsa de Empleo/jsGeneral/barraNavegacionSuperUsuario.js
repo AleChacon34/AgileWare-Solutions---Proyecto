abf0b2577c2f8e819035c08c9b381a769a510dcf
Jonah <jmorah@ucenfotec.ac.cr>
2023-08-01 15:36:49 -0600

    barra js Super lista

--- Bolsa de Empleo/jsGeneral/barraNavegacionSuperUsuario.js
+++ Bolsa de Empleo/jsGeneral/barraNavegacionSuperUsuario.js
@@ -14,62 +14,45 @@ function crearBarraNavegacionEmpresarial(){
     let addDivI = document.createElement('div');
     let addDivII = document.createElement('div');
     let addDivIII = document.createElement('div');
-    let addDivIV = document.createElement('div');
-    let addDivV = document.createElement('div');
-    //Add class for logout V
-    addDivV.classList.add("logout");
-    //Create button for V
+    //Add class for logout Div III
+    addDivIII.classList.add("logout");
+    //Create button for III
     let newBtn = document.createElement('button');
     newBtn.setAttribute('id', "logOut");
     //Create Icon I, II, III, IV, V
     let addIconI = document.createElement("i");
     addIconI.classList.add("fa-solid","fa-user");
     let addIconII = document.createElement("i");
-    addIconII.classList.add("fa-solid", "fa-briefcase");
+    addIconII.classList.add("fa-solid", "fa-arrow-up-right-from-square");
     let addIconIII = document.createElement("i");
-    addIconIII.classList.add("fa-solid", "fa-arrow-up-right-from-square");
-    let addIconIV = document.createElement("i");
-    addIconIV.classList.add("fa-solid", "fa-users");
-    let addIconV = document.createElement("i");
-    addIconV.classList.add("fa-solid", "fa-right-from-bracket");
+    addIconIII.classList.add("fa-solid", "fa-right-from-bracket");
     
     //Create anchor I, II, III, IV
     let newAnchI = document.createElement("a");
-    newAnchI.href = "/Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Sistema-Perfil Usuario Empresarial/Sistema-Perfil -Usuario-Empresarial.html";
+    newAnchI.href = "/";
     let linkAnchI = document.createTextNode("Perfil");
     newAnchI.appendChild(linkAnchI);
     let newAnchII = document.createElement("a");
-    newAnchII.href = "/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarListaOfertas.html";
-    let linkAnchII = document.createTextNode("Mis Empleos");
+    newAnchII.href = "/Bolsa de Empleo/Empresa - Reporteria/Consultar invitaciones enviadas/consultarInvitaciones.html";
+    let linkAnchII = document.createTextNode("Invitaciones");
     newAnchII.appendChild(linkAnchII);
-    let newAnchIII = document.createElement("a");
-    newAnchIII.href = "/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarListaOfertas.html";
-    let linkAnchIII = document.createTextNode("Reportes");
-    newAnchIII.appendChild(linkAnchIII);
-    let newAnchIV = document.createElement("a");
-    newAnchIV.href = "/Bolsa de Empleo/Empresa - Colaboradores/Visualizar Colaboradores/visualizarReclutadores.html";
-    let linkAnchIV = document.createTextNode("Colaboradores");
-    newAnchIV.appendChild(linkAnchIV);
     
     //Append Icon to Div and Btn
     addDivI.appendChild(addIconI);
     addDivII.appendChild(addIconII);
     addDivIII.appendChild(addIconIII);
-    addDivIV.appendChild(addIconIV);
-    newBtn.appendChild(addIconV);
+    newBtn.appendChild(addIconIII);
     //Append <a> to Div
     addDivI.appendChild(newAnchI);
     addDivII.appendChild(newAnchII);
-    addDivIII.appendChild(newAnchIII);
-    addDivIV.appendChild(newAnchIV);
+
     //Append Div to <nav>
     navBar.appendChild(addDivI);
     navBar.appendChild(addDivII);
     navBar.appendChild(addDivIII);
-    navBar.appendChild(addDivIV);
-    navBar.appendChild(addDivV);
+
     //Append button to Div
-    addDivV.appendChild(newBtn);
+    addDivIII.appendChild(newBtn);
     
     //Append NAV to BODY
     htmlBody.appendChild(navBar); 
@@ -87,7 +70,7 @@ function crearBarraNavegacionEmpresarial(){
     newScriptI.setAttribute("crossorigin","anonymous");
     newScriptII.setAttribute("src", "/Bolsa de Empleo/jsGeneral/cerrarSesion.js");
     newLinkI.setAttribute("rel", "stylesheet");
-    newLinkI.setAttribute("href", "/Bolsa de Empleo/cssGeneral/barraNavegacionEmpresa.css")
+    newLinkI.setAttribute("href", "/Bolsa de Empleo/cssGeneral/barraSuperUsuario.css")
     
     //Add <script> and <link> to head
     htmlHead.appendChild(newScriptI);
