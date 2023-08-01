0929125eea726e9aa6d540b54adc2bb3879c4660
Jonah <jmorah@ucenfotec.ac.cr>
2023-08-01 11:51:36 -0600

    barra buscador update

--- Bolsa de Empleo/jsGeneral/barraNavegacionBuscador.js
+++ Bolsa de Empleo/jsGeneral/barraNavegacionBuscador.js
@@ -11,9 +11,19 @@ function crearBarraNavegacion(){
     navBar.classList.add("navBar");
     //Append NAV to BODY
     htmlBody.appendChild(navBar);
+    //Create Div I
     let addDivI = document.createElement('div');
-    let addIcon = addDivI.appendChild(document.createElement("i"));
+    //Create Icon I
+    let addIcon = document.createElement("i");
     addIcon.classList.add("fa-solid","fa-user");
-
+    //Create anchor I
+    let newAnch = document.createElement("a");
+    newAnch.href = "/Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Perfil usuario/perfilUsuario.html";
+    let linkAnch = document.createTextNode("Perfil");
+    newAnch.appendChild(linkAnch);
+    
+    //Append Icon to Div
+    addDivI.appendChild(addIcon);
+    addDivI.appendChild(newAnch)
     navBar.appendChild(addDivI);
 }
