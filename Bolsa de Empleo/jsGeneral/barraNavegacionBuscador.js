e7efb9a73f169ff3948b73bcdabc8cbf1be3c70c
Jonah <jmorah@ucenfotec.ac.cr>
2023-08-01 12:15:39 -0600

    barra navegacin buscador lista

--- Bolsa de Empleo/jsGeneral/barraNavegacionBuscador.js
+++ Bolsa de Empleo/jsGeneral/barraNavegacionBuscador.js
@@ -11,17 +11,25 @@ function crearBarraNavegacion(){
     navBar.classList.add("navBar");
     //Append NAV to BODY
     htmlBody.appendChild(navBar);
-    //Create Div I, II, III
+    //Create Div I, II, III< IV
     let addDivI = document.createElement('div');
     let addDivII = document.createElement('div');
     let addDivIII = document.createElement('div');
-    //Create Icon I, II, III
+    let addDivIV = document.createElement('div');
+    //Add class for logout IV
+    addDivIV.classList.add("logout");
+    //Create button for IV
+    let newBtn = document.createElement('button');
+    newBtn.setAttribute('id', "logOut");
+    //Create Icon I, II, III, IV
     let addIconI = document.createElement("i");
     addIconI.classList.add("fa-solid","fa-user");
     let addIconII = document.createElement("i");
     addIconII.classList.add("fa-solid", "fa-briefcase");
     let addIconIII = document.createElement("i");
     addIconIII.classList.add("fa-solid", "fa-hand-pointer");
+    let addIconIV = document.createElement("i");
+    addIconIV.classList.add("fa-solid", "fa-right-from-bracket");
     //Create anchor I, II, III
     let newAnchI = document.createElement("a");
     newAnchI.href = "/Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Perfil usuario/perfilUsuario.html";
@@ -36,10 +44,11 @@ function crearBarraNavegacion(){
     let linkAnchIII = document.createTextNode("Postulaciones");
     newAnchIII.appendChild(linkAnchIII);
     
-    //Append Icon to Div
+    //Append Icon to Div and Btn
     addDivI.appendChild(addIconI);
     addDivII.appendChild(addIconII);
     addDivIII.appendChild(addIconIII);
+    newBtn.appendChild(addIconIV);
     //Append <a> to Div
     addDivI.appendChild(newAnchI);
     addDivII.appendChild(newAnchII);
@@ -48,4 +57,8 @@ function crearBarraNavegacion(){
     navBar.appendChild(addDivI);
     navBar.appendChild(addDivII);
     navBar.appendChild(addDivIII);
+    navBar.appendChild(addDivIV);
+    //Append button to Div
+    addDivIV.appendChild(newBtn);
+
 }
