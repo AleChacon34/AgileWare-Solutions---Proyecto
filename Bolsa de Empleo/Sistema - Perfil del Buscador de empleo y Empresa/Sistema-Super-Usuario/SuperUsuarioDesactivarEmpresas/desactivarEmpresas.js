fe6dbe108c86035aecf465d7c9da179a69f6087d
Jonah <jmorah@ucenfotec.ac.cr>
2023-08-02 17:39:46 -0600

    desactivar empresas

---
+++ Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Sistema-Super-Usuario/SuperUsuarioDesactivarEmpresas/desactivarEmpresas.js
@@ -0,0 +1,10 @@
+'use strict'
+
+document.addEventListener("DOMContentLoaded", () => {
+    let estadoBtn = document.querySelector("#estado-perfil")
+    estadoBtn.addEventListener('click', desactivarPerfil);
+})
+
+function desactivarPerfil(){
+    
+}
