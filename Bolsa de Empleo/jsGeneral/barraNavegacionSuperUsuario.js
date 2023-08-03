9988a351680295e6f4076e7c9641946b2a2a9023
Jonah <jmorah@ucenfotec.ac.cr>
2023-08-02 19:06:16 -0600

    mejoras barras

--- Bolsa de Empleo/jsGeneral/barraNavegacionSuperUsuario.js
+++ Bolsa de Empleo/jsGeneral/barraNavegacionSuperUsuario.js
@@ -14,8 +14,9 @@ function crearBarraNavegacionEmpresarial(){
     let addDivI = document.createElement('div');
     let addDivII = document.createElement('div');
     let addDivIII = document.createElement('div');
+    let addDivIV = document.createElement('div');
     //Add class for logout Div III
-    addDivIII.classList.add("logout");
+    addDivIV.classList.add("logout");
     //Create button for III
     let newBtn = document.createElement('button');
     newBtn.setAttribute('id', "logOut");
@@ -25,8 +26,9 @@ function crearBarraNavegacionEmpresarial(){
     let addIconII = document.createElement("i");
     addIconII.classList.add("fa-solid", "fa-arrow-up-right-from-square");
     let addIconIII = document.createElement("i");
-    addIconIII.classList.add("fa-solid", "fa-right-from-bracket");
-    
+    addIconIII.classList.add("fa-solid", "fa-envelope");
+    let addIconIV = document.createElement("i");
+    addIconIV.classList.add("fa-solid", "fa-right-from-bracket");
     //Create anchor I, II, III, IV
     let newAnchI = document.createElement("a");
     newAnchI.href = "/";
@@ -34,25 +36,32 @@ function crearBarraNavegacionEmpresarial(){
     newAnchI.appendChild(linkAnchI);
     let newAnchII = document.createElement("a");
     newAnchII.href = "/Bolsa de Empleo/Empresa - Reporteria/Consultar invitaciones enviadas/consultarInvitaciones.html";
-    let linkAnchII = document.createTextNode("Invitaciones");
+    let linkAnchII = document.createTextNode("Reportes");
     newAnchII.appendChild(linkAnchII);
+    let newAnchIII = document.createElement("a");
+    newAnchIII.href = "/";
+    let linkAnchIII = document.createTextNode("Invitaciones");
+    newAnchIII.appendChild(linkAnchIII);
     
     //Append Icon to Div and Btn
     addDivI.appendChild(addIconI);
     addDivII.appendChild(addIconII);
     addDivIII.appendChild(addIconIII);
-    newBtn.appendChild(addIconIII);
+
+    newBtn.appendChild(addIconIV);
     //Append <a> to Div
     addDivI.appendChild(newAnchI);
     addDivII.appendChild(newAnchII);
+    addDivIII.appendChild(newAnchIII);
 
     //Append Div to <nav>
     navBar.appendChild(addDivI);
     navBar.appendChild(addDivII);
     navBar.appendChild(addDivIII);
+    navBar.appendChild(addDivIV);
 
     //Append button to Div
-    addDivIII.appendChild(newBtn);
+    addDivIV.appendChild(newBtn);
     
     //Append NAV to BODY
     htmlBody.appendChild(navBar); 
