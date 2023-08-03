8f407b36abcfffdb88d419ea6bc5dcae14e4c283
Jonah <jmorah@ucenfotec.ac.cr>
2023-08-02 18:07:54 -0600

    desactivacion de empresa

--- Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Sistema-Super-Usuario/SuperUsuarioDesactivarEmpresas/desactivarEmpresas.js
+++ Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Sistema-Super-Usuario/SuperUsuarioDesactivarEmpresas/desactivarEmpresas.js
@@ -6,5 +6,22 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 function desactivarPerfil(){
-    
+
+    Swal.fire({
+        icon: "warning",
+        title: "Desactivación de usuario",
+        text: "La desactivación del perfil inhabilitará el inicio de sesión de este empresa y de sus usuarios empresariales.",
+        confirmButtonText: "Proceder",
+        showDenyButton: true,
+        denyButtonText: "Cancelar",
+    }).then((response) => {
+        Swal.fire({
+            icon: "success",
+            title: "Perfil desactivado con éxito",
+            showConfirmButton: false,
+            timer: 3500, 
+        }).then(() => {
+            window.location.replace("/Bolsa de Empleo/Empresa - Reporteria/Consultar invitaciones enviadas/consultarInvitaciones.html")
+        });
+    })   
 }
