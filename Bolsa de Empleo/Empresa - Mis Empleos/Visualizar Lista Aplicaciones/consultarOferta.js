1263a8491685fc856efe87c8166b637720f781a5
jmorahU96 <jmorah@ucenfotec.ac.cr>
2023-08-04 13:08:37 -0600

    consultar ofertas

--- Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarOferta.js
+++ Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarOferta.js
@@ -1,17 +1,20 @@
 'use strict'
 
-let estadoOferta = document.querySelector("#estado-oferta");
 
 document.addEventListener("DOMContentLoaded", ()=>{
+    let estadoOferta = document.querySelector(".estado-oferta");
+    console.log(estadoOferta);
     if (estadoOferta.textContent == "Oferta pública"){
-        console.log("Oferta Pública")
+        console.log("Oferta Pública");
+        estadoOferta.removeAttribute("id");
+        estadoOferta.setAttribute("id", "oferta-publica");
     }
     else if (estadoOferta.textContent == "Oferta oculta"){
+        estadoOferta.removeAttribute("id");
+        estadoOferta.setAttribute("id", "oferta-oculta");
         console.log("Oferta Oculta")
     }
     else{
         console.log("No funciona")
     }
 });
-
-module.exports(estadoOferta);
