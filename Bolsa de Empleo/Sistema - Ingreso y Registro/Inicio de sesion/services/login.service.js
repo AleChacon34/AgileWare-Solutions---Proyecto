5f6d2cd022638b427267eb7b5cadf55a21d807f5
AleChacon <achacon81.ac@gmail.com>
2023-08-07 13:35:48 -0600

    Login de usuario completo

---
+++ Bolsa de Empleo/Sistema - Ingreso y Registro/Inicio de sesion/services/login.service.js
@@ -0,0 +1,39 @@
+'use strict';
+
+export class BuscadorService {
+    static #URI = "http://localhost:3000";
+
+    static registerBuscador(newBuscador) {
+        return axios.post(
+            `${BuscadorService.#URI}/buscadores/register`,
+            {
+                nombreBuscador: newBuscador.nombreBuscador,
+                apellidosBuscador: newBuscador.apellidosBuscador,
+                correoBuscador: newBuscador.contraseniaBuscador,
+                telefonoBuscador: newBuscador.telefonoBuscador,
+                generoBuscador: newBuscador.generoBuscador,
+                contraseniaBuscador: newBuscador.contraseniaBuscador
+            }, 
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+    }
+
+    static loginBuscador(newBuscador) {
+        return axios.post(
+            `${BuscadorService.#URI}/buscadores/login`,
+            {
+                correoBuscador: newBuscador.correoBuscador,
+                contraseniaBuscador: newBuscador.contraseniaBuscador
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+    }
+}
