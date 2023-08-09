e4ec603171a9b4ae7b40a752b85a08969a899364
Tadakatsuo <solcristopher1105@gmail.com>
2023-08-09 14:56:13 -0600

    updates

--- BL/routes/user.routes.js
+++ BL/routes/user.routes.js
@@ -4,6 +4,33 @@ const express = require('express')
 const router = express.Router(); 
 const userControllers = require('../controllers/user.controller');
 
+
 //Get Usuario//
 
-router.get("/", userControllers.getUsuario);
+router.get('/',userControllers.getUsuario);
+
+//Get Usuario id//
+
+router.get('/:id', userControllers.getUsuarioID);
+
+//Register Usuario//
+
+router.get('/register', userControllers.registerUser);
+
+//Login Usuario//
+
+router.get('/login', userControllers.loginUser);
+
+//Put Usuario//
+
+router.get('/put', userControllers.putUsuario);
+
+//Delete Usuario//
+
+router.get('/delete', userControllers.deleteUsuario);
+
+//Update Usuario//
+
+router.get('/update', userControllers.updateUsuario);
+
+module.exports = router;
