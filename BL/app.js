038190f626ffa0a82c70612de1d0ab3d8d0cad7e
AleChacon <achacon81.ac@gmail.com>
2023-08-08 19:07:08 -0600

    Update

--- BL/app.js
+++ BL/app.js
@@ -11,6 +11,7 @@ const buscadorRoutes = require('./routes/buscador.routes');
 const empresaRoutes = require('./routes/empresa.routes');
 const usuEmpresarialRoutes = require('./routes/usuEmpresarial.routes');
 const ofertasRoutes = require("./routes/ofertas.routes");
+const superUserRoutes = require('./routes/superUser.routes');
 
 app.use(cors());
 app.use(body_parser.json());
@@ -36,3 +37,4 @@ app.use('/buscadores', buscadorRoutes);
 app.use('/empresa',empresaRoutes);
 app.use('/usuEmpresarial',usuEmpresarialRoutes);
 app.use("/ofertas", ofertasRoutes);
+app.use('/superUser', superUserRoutes);
---
+++ BL/controllers/auth.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+/**
+ * REGISTER SUPERUSER
+ */
+router.post('/register',);
+
+/**
+ * LOGIN SUPERUSER
+ */
+router.post('/login', );
