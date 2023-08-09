c931f8e04bd311a5a7505476bcd8ff83424fb40f
Jonah <jmorah@ucenfotec.ac.cr>
2023-08-08 23:22:09 -0600

    update

--- Bolsa de Empleo/Empresa - Mis Empleos/Crear Oferta de Empleo/crearOfertaEmpleo.js
+++ Bolsa de Empleo/Empresa - Mis Empleos/Crear Oferta de Empleo/crearOfertaEmpleo.js
@@ -3,37 +3,61 @@
 // Asignamiento de alerta para los botones de Publicar y Ocultar Oferta
 
 document.addEventListener("DOMContentLoaded", () =>{
-    let publicarBtn = document.querySelector("button[name='publicar']");
-    let ocultarBtn = document.querySelector("button[name='ocultar']");
-    publicarBtn.addEventListener("click", publicarOferta);
-    ocultarBtn.addEventListener("click", ocultarOferta);
-    let cuadroTitulo= document.querySelector("#seccionTitulo");
-    console.log(cuadroTitulo);
+   
+    // let ocultarBtn = document.querySelector("button[name='ocultar']");
+    let form = document.querySelector("#ofertaForm");
+    form.addEventListener("submit", postOferta);
 })
 
-function publicarOferta(){
-    Swal.fire({
-        customClass: {popup: "swal"},
-        icon: 'success',
-        title: 'Oferta publicada con éxito',
-        confirmButtonText: 'Continuar',
-      }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.replace("/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarListaOfertas.html")
-        };
-      });
-}
+// function publicarOferta(e){
+//     e.preventDefault(e.target);
+//     Swal.fire({
+//         customClass: {popup: "swal"},
+//         icon: 'success',
+//         title: 'Oferta publicada con éxito',
+//         confirmButtonText: 'Continuar',
+//       }).then((result) => {
+//         if (result.isConfirmed) {
+//             window.location.replace("/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarListaOfertas.html")
+//         };
+//       });
+// }
 
-function ocultarOferta(){
-    console.log("oculta")
+// function ocultarOferta(e){
+//     e.preventDefault(e.target);
+//     console.log("oculta")
+//     Swal.fire({
+//         customClass: {popup: "swal"},
+//         icon: 'success',
+//         title: 'Oferta ocultada con éxito',
+//         confirmButtonText: 'Continuar',
+//       }).then((result) => {
+//         if (result.isConfirmed) {
+//             window.location.replace("/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarListaOfertas.html")
+//         }
+//       });
+// }
+
+function postOferta(){
+  let publicarBtn = document.querySelector("button[name='publicar']");
+  let cuadroTitulo= document.querySelector("#seccionTitulo");
+  let cuadroRequerimientos= document.querySelector("#seccionRequerimientos");
+  let cuadroDescripcion= document.querySelector("#seccionDescripcion");
+
+  if ((cuadroTitulo.value === "" && cuadroRequerimientos.value === "" && cuadroDescripcion.value === "")){
+    publicarBtn.disabled = true;
+    console.log("Vacio")
+  }
+  else{
     Swal.fire({
-        customClass: {popup: "swal"},
-        icon: 'success',
-        title: 'Oferta ocultada con éxito',
-        confirmButtonText: 'Continuar',
-      }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.replace("/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarListaOfertas.html")
-        }
-      });
+      customClass: {popup: "swal"},
+      icon: 'success',
+      title: 'Oferta publicada con éxito',
+      confirmButtonText: 'Continuar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+          window.location.replace("/Bolsa de Empleo/Empresa - Mis Empleos/Visualizar Lista Aplicaciones/consultarListaOfertas.html")
+      };
+    });
+  }
 }
