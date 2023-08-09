7a4ba6af33812b61da398472b230fdad57651504
jaquisg <jaquisolisg@gmail.com>
2023-08-09 14:56:12 -0600

    cambios

--- BL/controllers/user.controller.js
+++ BL/controllers/user.controller.js
@@ -1,3 +1,37 @@
 'use strict';
 
 const userModel = require("../models/user.model");
+
+//GET
+
+async function getUser(request, response){
+    const data = await userModel.find({});
+    response.send({data});
+};
+
+//POST
+
+async function postUser(request, response){
+    const {body} = request;
+    const data = await userModel.create(body);
+    response.send({data});
+};
+
+//PUT
+async function putUser(request, response){
+    const {body} = request;
+    const {_id} = body;
+    const data = await userModel.findOneAndUpdate({_id}, body);
+    response.send({data});
+};
+
+//DELETE
+
+async function deleteUser(request, response){
+    const {body} = request;
+    const {_id} = body;
+    const data = await userModel.findOneAndDelete({_id});
+    response.send({data});
+};
+
+module.exports={getUser,postUser,putUser,deleteUser};
