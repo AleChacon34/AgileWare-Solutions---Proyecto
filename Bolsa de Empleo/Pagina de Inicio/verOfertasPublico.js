d15f091c99e8fde88f9e3f8d736f09e3ebed57c6
jmorahU96 <jmorah@ucenfotec.ac.cr>
2023-08-15 09:54:57 -0600

    updates

--- Bolsa de Empleo/Pagina de Inicio/verOfertasPublico.js
+++ Bolsa de Empleo/Pagina de Inicio/verOfertasPublico.js
@@ -3,6 +3,8 @@
 import { OfertaService } from "../services/consultarOferta.services.js"
 
 document.addEventListener("DOMContentLoaded", ()=>{
+    let form = document.querySelector("#busquedaForm");
+    form.addEventListener("submit", filtrarOfertas)
     getOfertas();
 })
 
@@ -82,4 +84,66 @@ function pedirIngreso(){
             location.href="/Bolsa de Empleo/Sistema - Ingreso y Registro/Inicio de sesion/inicioSesion.html";
         }
     })
+}
+
+function filtrarOfertas(e){
+    let paramBusqueda =  document.querySelector("#barraBusqueda");
+    e.preventDefault(e.target);
+    OfertaService.filter(paramBusqueda.value).then((response) => {
+        let data = response.data.data;
+        if(data == ""){
+            Swal.fire({
+                icon: "info",
+                title: "Búsqueda",
+                text: "No se encontraron resultados con los criterios brindados, por favor inténtelo de nuevo"
+            })
+            paramBusqueda.value = "";
+        }
+        else{
+            data.forEach(mostrarResultados);
+        }   
+    })
+}
+
+function mostrarResultados(data, paramBusqueda){
+    if (paramBusqueda.value != ""){
+        let mainBody = document.querySelector("main");
+        for ( let div = 1; div < mainBody.children.length; div++ ){
+            mainBody.children[div].replaceChildren();
+        }
+        if (data.visibilidad === "Activa" && data.estadoOferta != "Oferta oculta"){
+            console.log('hola')
+            //Crear DIV principal con id
+            let addDiv = document.createElement('div');
+            //Crear section del div
+            let addSec = document.createElement('section');
+            addSec.setAttribute("id", data._id)
+            //Crear h3, h4, textarea, anchor, p 
+            let addH3I = document.createElement('h3');
+            addH3I.setAttribute("id", "tituloOFerta");
+            let addH4I = document.createElement('h4');
+            addH4I.setAttribute("id", "requerimientos");
+            let textareaI = document.createElement('textarea');
+            textareaI.setAttribute("id", "requerimientosCuerpo");
+            let addH4II = document.createElement('h4');
+            addH4II.setAttribute("id", "descripcionOferta");
+            let textareaII = document.createElement('textarea');
+            textareaII.setAttribute("id", "descripcionCuerpo");
+            let addAside = document.createElement('aside');
+            let img = document.createElement('img');
+            img.setAttribute("src", "#");
+            //Agregar texto a cada elemento desde datosOfertas
+            addH3I.appendChild(document.createTextNode(data.seccionTitulo));
+            addH4I.appendChild(document.createTextNode("Requerimientos"));
+            textareaI.appendChild(document.createTextNode(data.seccionRequerimientos));
+            addH4II.appendChild(document.createTextNode("Descripción"));
+            textareaII.appendChild(document.createTextNode(data.seccionDescripcion));
+            //Agregar los H3, H4 y textarea al section, y este al Div
+            addSec.append(addH3I, addH4I, textareaI, addH4II, textareaII);
+            addDiv.appendChild(addAside, addSec);
+            //Agregar Div al main
+            let mainBody = document.querySelector('main');
+            mainBody.insertBefore(addDiv, mainBody.children[1]);
+        }
+    }
 }
