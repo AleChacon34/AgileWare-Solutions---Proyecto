247acd8918ca06350e4bc9290f2413a9eecb0aa3
jmorahU96 <jmorah@ucenfotec.ac.cr>
2023-08-15 13:14:40 -0600

    updates

--- Bolsa de Empleo/Empresa - Reporteria/Consultar invitaciones enviadas/consultarInvitaciones.js
+++ Bolsa de Empleo/Empresa - Reporteria/Consultar invitaciones enviadas/consultarInvitaciones.js
@@ -9,51 +9,77 @@ document.addEventListener("DOMContentLoaded", () =>{
 
 function imprimirReporte(){
     let opcionSeleccion = document.querySelector("#seleccionReporte");
-    //Borrar el cuerpo de la tabla ya existente en el HTML
-    let crearTabla = document.querySelector(".tabla-datos tbody");
-    crearTabla.innerHTML = "";
-    UserService.filter("Empresa").then((response)=>{
-        let data =  response.data.data;
-        console.log(data)
-    })
+    
+    //Ver reporte de empresas
+    if(opcionSeleccion.value === "REPORTE DE EMPRESAS"){
+        UserService.filter("Empresa").then((response)=>{
+            let data =  response.data.data;
+            data.forEach((data)=>{ 
+                //Borrar el cuerpo de la tabla ya existente en el HTML
+                let crearTabla = document.querySelector(".tabla-datos tbody");
+                crearTabla.innerHTML = "";
+                //Crear el Table Header
+                let headRowI = crearTabla.insertRow(0)
+                headRowI.insertCell(0).outerHTML ="<th>Nombre de la Empresa</th>";
+                headRowI.insertCell(1).outerHTML ="<th>Correo Electrónico</th>";
+                headRowI.insertCell(2).outerHTML ="<th>Estado del perfil</th>";
 
-    // Diferentes tipos de reportes
-    switch (opcion.value){
-        case "INVITACIONES ENVIADAS":
-            //Crear el Table Header
-            let headRowI = crearTabla.insertRow(0)
-            headRowI.insertCell(0).outerHTML ="<th>Nombre de la Empresa</th>";
-            headRowI.insertCell(1).outerHTML ="<th>Correo Electrónico</th>";
-            headRowI.insertCell(2).outerHTML ="<th>Correo Emisor</th>";
-
-            //Crear las filas con la informacion de los objetos
-            for(let dato = 0;  dato < listaDatos.length; dato++){
+                //Crear las filas con la informacion de los objetos
                 let row = crearTabla.insertRow();
-                row.insertCell(0).appendChild(document.createTextNode(listaDatos[dato].nombreEmpresa));
-                row.insertCell(1).appendChild(document.createTextNode(listaDatos[dato].correoEmpresa));
-                row.insertCell(2).appendChild(document.createTextNode(listaDatos[dato].correoEmisor));
-            }
-            break;
-        case "REPORTE DE EMPRESAS":
-            //Crear el Table Header
-            let headRowR = crearTabla.insertRow(0)
-            headRowR.insertCell(0).outerHTML ="<th>Nombre de la Empresa</th>";
-            headRowR.insertCell(1).outerHTML ="<th>Correo Electrónico</th>";
-            headRowR.insertCell(2).outerHTML ="<th>Estado perfil</th>";
+                row.insertCell(0).appendChild(document.createTextNode(data.nombre));
+                row.insertCell(1).appendChild(document.createTextNode(data.correo));
+                let estadoSelect = document.createElement("select");
+                estadoSelect.setAttribute("id", "estadoSelect")
+                let estadoOptionI = document.createElement("option");
+                estadoOptionI.appendChild(document.createTextNode(data.visibilidad));
+                let estadoOptionII = document.createElement("option");
+                if (data.visibilidad === "Activa"){
+                    estadoOptionII.appendChild(document.createTextNode("Inactiva"));
+                }
+                else if (data.visibilidad === "Inactiva"){
+                    estadoOptionII.appendChild(document.createTextNode("Activa"));
+                }
+                estadoSelect.append(estadoOptionI, estadoOptionII);
+                row.insertCell(2).appendChild(estadoSelect);
+            })
+        })
+    }
+    
+    //Ver reporte de Administradores
+    if(opcionSeleccion.value === "REPORTE DE ADMINISTRADORES"){
+        UserService.filter("Administrador").then((response)=>{
+            let data =  response.data.data;
+            data.forEach((data)=>{ 
+                //Borrar el cuerpo de la tabla ya existente en el HTML
+                let crearTabla = document.querySelector(".tabla-datos tbody");
+                crearTabla.innerHTML = "";
+                //Crear el Table Header
+                let headRowI = crearTabla.insertRow(0)
+                headRowI.insertCell(0).outerHTML ="<th>Nombre de la Empresa</th>";
+                headRowI.insertCell(1).outerHTML ="<th>Correo Electrónico</th>";
+                headRowI.insertCell(2).outerHTML ="<th>Pertenencia</th>";
+                headRowI.insertCell(3).outerHTML ="<th>Estado del perfil</th>";
 
-            //Crear las filas con la informacion de los objetos
-            for(let dato = 0;  dato < listaDatos.length; dato++){
+                //Crear las filas con la informacion de los objetos
                 let row = crearTabla.insertRow();
-                row.insertCell(0).appendChild(document.createTextNode(listaDatos[dato].nombreEmpresa));
-                row.insertCell(1).appendChild(document.createTextNode(listaDatos[dato].correoEmpresa));
-                // Crear enlace a cambio de estado de empresa
-                let newAnch = document.createElement("a");
-                newAnch.setAttribute("href", "/Bolsa de Empleo/Sistema - Perfil del Buscador de empleo y Empresa/Sistema-Super-Usuario/SuperUsuarioDesactivarEmpresas/desactivarEmpresas.html")
-                newAnch.appendChild(document.createTextNode(listaDatos[dato].estadoEmpresa));
-                row.insertCell(2).appendChild(newAnch);
-            }
-            break;
-        default:
-            break;
+                row.insertCell(0).appendChild(document.createTextNode(data.nombre));
+                row.insertCell(1).appendChild(document.createTextNode(data.correo));
+                row.insertCell(2).appendChild(document.createTextNode(data.pertenencia));
+                let estadoSelect = document.createElement("select");
+                estadoSelect.setAttribute("id", "estadoSelect")
+                let estadoOptionI = document.createElement("option");
+                estadoOptionI.appendChild(document.createTextNode(data.visibilidad));
+                let estadoOptionII = document.createElement("option");
+                if (data.visibilidad === "Activa"){
+                    estadoOptionII.appendChild(document.createTextNode("Inactiva"));
+                }
+                else if (data.visibilidad === "Inactiva"){
+                    estadoOptionII.appendChild(document.createTextNode("Activa"));
+                }
+                estadoSelect.append(estadoOptionI, estadoOptionII);
+                row.insertCell(3).appendChild(estadoSelect);
+                
+            })
+        })
     }
 }
