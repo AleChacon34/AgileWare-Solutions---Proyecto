8b65095200aaed76ad7ece64adf60a4da965a4a6
Jonah <jmorah@ucenfotec.ac.cr>
2023-08-16 12:27:13 -0600

    updates

--- Bolsa de Empleo/Buscador de Empleo - Postulaciones/Ver Postulaciones/verPostulaciones.js
+++ Bolsa de Empleo/Buscador de Empleo - Postulaciones/Ver Postulaciones/verPostulaciones.js
@@ -1,11 +1,18 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    generarPostulaciones;
+})
+
+
 /**
  * Al cargar la pagina, se le asigna el evento a los botones
  */
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("loadstart", () => {
     let btns = document.querySelectorAll('.verPostulacion');
     for (let i = 0; i < btns.length; i++) {
         let btn = btns[i];
-        btn.onclick = redirect;
+        btn.addEventListener("click", redirect);
     }
 });
 
@@ -14,4 +21,17 @@ document.addEventListener('DOMContentLoaded', () => {
  */
 function redirect() {
     window.location.href = "postulacion.html";
+}
+
+function generarPostulaciones(){
+    let addSec = document.createElement("section");
+    let mainDiv = document.createElement("div");
+    mainDiv.setAttribute("class", "card");
+    let addDivI = document.createElement("div");
+    let addDivII = document.createElement("div");
+    let addH2 = document.createElement("h2");
+    let addBtn = document.createElement("button");
+    let parI =  document.createElement("p");
+    let parII =  document.createElement("p");
+    let parIII =  document.createElement("p");
 }
