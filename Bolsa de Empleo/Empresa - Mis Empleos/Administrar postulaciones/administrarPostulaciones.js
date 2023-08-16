77826f8dca76a2718896ac7bcbb6d4099e5e603d
Jonah <jmorah@ucenfotec.ac.cr>
2023-08-16 10:42:15 -0600

    update

--- Bolsa de Empleo/Empresa - Mis Empleos/Administrar postulaciones/administrarPostulaciones.js
+++ Bolsa de Empleo/Empresa - Mis Empleos/Administrar postulaciones/administrarPostulaciones.js
@@ -1,34 +1,25 @@
 'use strict';
 
-let datosInvitaciones = [
+let datosInvitaciones =
     {
-        nombreBuscador: "Jonathan", apellidos: "Mora Herra", correo: "jmorah@correo.com", estadoPostulacion: "Enviada"
-    },
+        nombreBuscador: "Jonathan", apellidos: "Mora Herra", correo: "jmorah@correo.com", tituloOferta: "Oferta en papas", estadoPostulacion: "Enviada"
+    }
 
-    {
-        nombreBuscador: "Marco", apellidos: "Mora Herra", correo: "jmorah@correo.com", estadoPostulacion: "En revisión"
-    },
 
-    {
-        nombreBuscador: "Cristopher", apellidos: "Mora Herra", correo: "jmorah@correo.com", estadoPostulacion: "Rechazada"
-    },
-
-    {
-        nombreBuscador: "Jacquelin", apellidos: "Mora Herra", correo: "jmorah@correo.com", estadoPostulacion: "Aceptada"
-    },
-];
 
 document.addEventListener("DOMContentLoaded", ()=>{
     let form = document.querySelector("#ofertaForm");
     generarPostulaciones(datosInvitaciones);
 })
 
-function generarPostulaciones(){
+function generarPostulaciones(datosInvitaciones){
     //Crear elementos
     let addDiv =  document.createElement("div");
-    let addDivI = document.createElement("div");
+    let addSecI = document.createElement("section");
+    let addSecII = document.createElement("section");
     let parI =  document.createElement("p");
     let parII =  document.createElement("p");
+    let parIII =  document.createElement("p");
     let select = document.createElement("select");
     let optionI = document.createElement("option");
     let optionII = document.createElement("option");
@@ -36,12 +27,19 @@ function generarPostulaciones(){
     let optionIV = document.createElement("option");
 
     //Agregar contenido
-    parI.appendChild(document.createTextNode(nombreBuscador + " " + apellidos));
-    parII.appendChild(document.createTextNode(correo));
+    parI.appendChild(document.createTextNode("Postulante: " + datosInvitaciones.nombreBuscador + " " + datosInvitaciones.apellidos));
+    parII.appendChild(document.createTextNode("Correo electrónico: " + datosInvitaciones.correo));
+    parIII.appendChild(document.createTextNode("Título de la oferta: " + datosInvitaciones.tituloOferta));
     optionI.appendChild(document.createTextNode("Enviada"));
     optionII.appendChild(document.createTextNode("En revisión"));
     optionIII.appendChild(document.createTextNode("Aceptada"));
     optionIV.appendChild(document.createTextNode("Denegada"));
 
-
+    //Agrupar elementos
+    addSecI.append(parI, parII, parIII);
+    select.append(optionI, optionII, optionIII, optionIV);
+    addSecII.append(select);
+    addDiv.append(addSecI, addSecII);
+    let mainBody = document.querySelector("main");
+    mainBody.appendChild(addDiv);
 }
