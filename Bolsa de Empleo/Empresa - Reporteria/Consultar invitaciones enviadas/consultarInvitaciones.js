bcc7117638b2cec6152c0c9837f023fe667ed92d
Jonah <jmorah@ucenfotec.ac.cr>
2023-08-18 12:09:29 -0600

    update

--- Bolsa de Empleo/Empresa - Reporteria/Consultar invitaciones enviadas/consultarInvitaciones.js
+++ Bolsa de Empleo/Empresa - Reporteria/Consultar invitaciones enviadas/consultarInvitaciones.js
@@ -48,37 +48,96 @@ function imprimirReporte(){
     //Ver reporte de Administradores
     if(opcionSeleccion.value === "REPORTE DE ADMINISTRADORES"){
         UserService.filter("Administrador").then((response)=>{
+            //Borrar el cuerpo de la tabla ya existente en el HTML
+            let crearTabla = document.querySelector(".tabla-datos tbody");
+            crearTabla.innerHTML = "";
+            //Crear el Table Header
+            let headRowI = crearTabla.insertRow(0)
+            headRowI.insertCell(0).outerHTML ="<th>Nombre de la Empresa</th>";
+            headRowI.insertCell(1).outerHTML ="<th>Correo Electrónico</th>";
+            headRowI.insertCell(2).outerHTML ="<th>Pertenencia</th>";
+            headRowI.insertCell(3).outerHTML ="<th>Fecha de creación</th>";
             let data =  response.data.data;
+            console.log(data)
             data.forEach((data)=>{ 
-                //Borrar el cuerpo de la tabla ya existente en el HTML
-                let crearTabla = document.querySelector(".tabla-datos tbody");
-                crearTabla.innerHTML = "";
-                //Crear el Table Header
-                let headRowI = crearTabla.insertRow(0)
-                headRowI.insertCell(0).outerHTML ="<th>Nombre de la Empresa</th>";
-                headRowI.insertCell(1).outerHTML ="<th>Correo Electrónico</th>";
-                headRowI.insertCell(2).outerHTML ="<th>Pertenencia</th>";
-                headRowI.insertCell(3).outerHTML ="<th>Estado del perfil</th>";
-
                 //Crear las filas con la informacion de los objetos
                 let row = crearTabla.insertRow();
                 row.insertCell(0).appendChild(document.createTextNode(data.nombre));
                 row.insertCell(1).appendChild(document.createTextNode(data.correo));
                 row.insertCell(2).appendChild(document.createTextNode(data.pertenencia));
-                let estadoSelect = document.createElement("select");
-                estadoSelect.setAttribute("id", "estadoSelect")
-                let estadoOptionI = document.createElement("option");
-                estadoOptionI.appendChild(document.createTextNode(data.visibilidad));
-                let estadoOptionII = document.createElement("option");
-                if (data.visibilidad === "Activa"){
-                    estadoOptionII.appendChild(document.createTextNode("Inactiva"));
-                }
-                else if (data.visibilidad === "Inactiva"){
-                    estadoOptionII.appendChild(document.createTextNode("Activa"));
-                }
-                estadoSelect.append(estadoOptionI, estadoOptionII);
-                row.insertCell(3).appendChild(estadoSelect);
-                
+                row.insertCell(3).appendChild(document.createTextNode((data.createdAt.substring(0, 10))));
+            })
+        })
+    }
+
+    if(opcionSeleccion.value === "REPORTE DE MANAGERS"){
+        UserService.filter("Manager").then((response)=>{
+            //Borrar el cuerpo de la tabla ya existente en el HTML
+            let crearTabla = document.querySelector(".tabla-datos tbody");
+            crearTabla.innerHTML = "";
+            //Crear el Table Header
+            let headRowI = crearTabla.insertRow(0)
+            headRowI.insertCell(0).outerHTML ="<th>Nombre de la Empresa</th>";
+            headRowI.insertCell(1).outerHTML ="<th>Correo Electrónico</th>";
+            headRowI.insertCell(2).outerHTML ="<th>Pertenencia</th>";
+            headRowI.insertCell(3).outerHTML ="<th>Fecha de creación</th>";
+            let data =  response.data.data;
+            console.log(data)
+            data.forEach((data)=>{ 
+                //Crear las filas con la informacion de los objetos
+                let row = crearTabla.insertRow();
+                row.insertCell(0).appendChild(document.createTextNode(data.nombre));
+                row.insertCell(1).appendChild(document.createTextNode(data.correo));
+                row.insertCell(2).appendChild(document.createTextNode(data.pertenencia));
+                row.insertCell(3).appendChild(document.createTextNode((data.createdAt.substring(0, 10))));
+            })
+        })
+    }
+
+    if(opcionSeleccion.value === "REPORTE DE MANAGERS"){
+        UserService.filter("Reclutador").then((response)=>{
+            //Borrar el cuerpo de la tabla ya existente en el HTML
+            let crearTabla = document.querySelector(".tabla-datos tbody");
+            crearTabla.innerHTML = "";
+            //Crear el Table Header
+            let headRowI = crearTabla.insertRow(0)
+            headRowI.insertCell(0).outerHTML ="<th>Nombre de la Empresa</th>";
+            headRowI.insertCell(1).outerHTML ="<th>Correo Electrónico</th>";
+            headRowI.insertCell(2).outerHTML ="<th>Pertenencia</th>";
+            headRowI.insertCell(3).outerHTML ="<th>Fecha de creación</th>";
+            let data =  response.data.data;
+            console.log(data)
+            data.forEach((data)=>{ 
+                //Crear las filas con la informacion de los objetos
+                let row = crearTabla.insertRow();
+                row.insertCell(0).appendChild(document.createTextNode(data.nombre));
+                row.insertCell(1).appendChild(document.createTextNode(data.correo));
+                row.insertCell(2).appendChild(document.createTextNode(data.pertenencia));
+                row.insertCell(3).appendChild(document.createTextNode((data.createdAt.substring(0, 10))));
+            })
+        })
+    }
+
+    if(opcionSeleccion.value === "REPORTE DE MANAGERS"){
+        UserService.filter("buscador").then((response)=>{
+            //Borrar el cuerpo de la tabla ya existente en el HTML
+            let crearTabla = document.querySelector(".tabla-datos tbody");
+            crearTabla.innerHTML = "";
+            //Crear el Table Header
+            let headRowI = crearTabla.insertRow(0)
+            headRowI.insertCell(0).outerHTML ="<th>Nombre de la Empresa</th>";
+            headRowI.insertCell(1).outerHTML ="<th>Correo Electrónico</th>";
+            headRowI.insertCell(2).outerHTML ="<th>Pertenencia</th>";
+            headRowI.insertCell(3).outerHTML ="<th>Fecha de creación</th>";
+            let data =  response.data.data;
+            console.log(data)
+            data.forEach((data)=>{ 
+                //Crear las filas con la informacion de los objetos
+                let row = crearTabla.insertRow();
+                row.insertCell(0).appendChild(document.createTextNode(data.nombre));
+                row.insertCell(1).appendChild(document.createTextNode(data.correo));
+                row.insertCell(2).appendChild(document.createTextNode(data.pertenencia));
+                row.insertCell(3).appendChild(document.createTextNode((data.createdAt.substring(0, 10))));
             })
         })
     }
