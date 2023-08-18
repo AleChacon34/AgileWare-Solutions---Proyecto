4eaf4a0e0813aa79ef2308b571c1b00ab7fb1c39
Jonah <jmorah@ucenfotec.ac.cr>
2023-08-18 10:15:36 -0600

    update

--- Bolsa de Empleo/Empresa - Mis Empleos/Consultar aplicaciones recibidas/listaPostulados.js
+++ Bolsa de Empleo/Empresa - Mis Empleos/Consultar aplicaciones recibidas/listaPostulados.js
@@ -10,8 +10,9 @@ document.addEventListener("DOMContentLoaded", () =>{
 
 function imprimirReporte(){
     let opcionSeleccion = document.querySelector("#seleccion-reporte");
+    let publicador =  localStorage.getItem("activeUser");
 
-    OfertaService.findAll().then((response)=>{
+    OfertaService.findByPublicador(publicador).then((response)=>{
         let data = response.data.data;
         //Borrar el cuerpo de la tabla ya existente en el HTML
         let crearTabla = document.querySelector(".tabla-datos tbody");
@@ -29,11 +30,11 @@ function imprimirReporte(){
 
             data.forEach((data)=>{
                 //Crear las filas con la informacion de los objetos
-                if (data.visibilidad === "Activa" && data.publicador === "Empresa"){
+                if (data.visibilidad === "Activa"){
                     let row = crearTabla.insertRow();
-                    row.insertCell(0).appendChild(document.createTextNode(data._id));
+                    row.insertCell(0).appendChild(document.createTextNode((data._id.substring(3,8))));
                     row.insertCell(1).appendChild(document.createTextNode(data.seccionTitulo));
-                    row.insertCell(2).appendChild(document.createTextNode(data.createdAt));
+                    row.insertCell(2).appendChild(document.createTextNode((data.createdAt).substring(0, 10)));
                     row.insertCell(3).appendChild(document.createTextNode(data.estadoOferta));   
                 }
             });
@@ -50,11 +51,13 @@ function imprimirReporte(){
 
             data.forEach((data)=>{
                 //Crear las filas con la informacion de los objetos
-                if (data.visibilidad === "Activa" && data.publicador === "Empresa"){
+                let pertenencia =  localStorage.getItem("activeUser");
+                console.log(data.pertenencia)
+                if (data.visibilidad === "Activa" && data.pertenencia === pertenencia){
                     let row = crearTabla.insertRow();
                     row.insertCell(0).appendChild(document.createTextNode(data._id));
                     row.insertCell(1).appendChild(document.createTextNode(data.seccionTitulo));
-                    row.insertCell(2).appendChild(document.createTextNode(data.createdAt));
+                    row.insertCell(2).appendChild(document.createTextNode((data.createdAt.substring(0, 10))));
                     row.insertCell(3).appendChild(document.createTextNode(data.estadoOferta));   
                 }
             });
@@ -116,9 +119,8 @@ function imprimirReporte(){
             headRowO.insertCell(2).outerHTML ="<th>Correo electrónico</th>";
 
             data.forEach((data)=>{
-                console.log(data.visibilidad)
                 //Crear las filas con la informacion de los objetos
-                if (data.visibilidad === "Activa" && data.rol === "Manager" && data.pertenencia === "Plataforma"){
+                if (data.visibilidad === "Activa" && data.rol === "buscador"){
                     let row = crearTabla.insertRow();
                     row.insertCell(0).appendChild(document.createTextNode(data._id));
                     row.insertCell(1).appendChild(document.createTextNode(data.nombre + " " + data.apellidos));
